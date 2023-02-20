65f55e633de88e6496ac564e271067a171900289
Ppaaoo <62310221+Ppaaoo@users.noreply.github.com>
2023-02-20 09:48:46 +0100

    Java project initialized

---
+++ data-access-with-jdbc/src/test/java/com/example/dataaccesswithjdbc/DataAccessWithJdbcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dataaccesswithjdbc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataAccessWithJdbcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
